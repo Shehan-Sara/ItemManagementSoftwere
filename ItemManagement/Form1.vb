b2ac1292ea7976c191b498e47d76f136def26091
Shehan Rashmika <116863196+Shehan-Sara@users.noreply.github.com>
2023-10-29 07:56:23 +0530

    Position changes fixed

--- ItemManagement/Form1.vb
+++ ItemManagement/Form1.vb
@@ -1,4 +1,15 @@
 ﻿Public Class Form1
+
+    Public Sub New()
+        InitializeComponent()
+
+        ' Set the StartPosition property to Manual
+        Me.StartPosition = FormStartPosition.Manual
+
+        ' Set the initial location of the form
+        Me.Location = New Point(100, 100) ' Specify the X and Y coordinates where you want the form to appear
+    End Sub
+
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
     End Sub
--- ItemManagement/Form2.vb
+++ ItemManagement/Form2.vb
@@ -1,4 +1,15 @@
 ﻿Public Class Form2
+
+    Public Sub New()
+        InitializeComponent()
+
+        ' Set the StartPosition property to Manual
+        Me.StartPosition = FormStartPosition.Manual
+
+        ' Set the initial location of the form
+        Me.Location = New Point(100, 100) ' Specify the X and Y coordinates where you want the form to appear
+    End Sub
+
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         Dim FirstForm As New Form1()
 
